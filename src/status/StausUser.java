9cb4adf27ed486247a8771d017080863ec402b39
logitechg502mouse <lader@sheridancollege.ca>
2022-02-23 13:45:23 -0500

    version 4

--- src/status/StausUser.java
+++ src/status/StausUser.java
@@ -4,31 +4,54 @@
  * and open the template in the editor.
  */
 package status;
-
+import java.util.Scanner;
 /**
  * MidTerm Exam
  * @author srinivsi
+ * @modifier Derrick La jdk17 feb23
  */
-public class StausUser 
-{
-   public void statusDetail(String code)
-{
-switch(code.toUpperCase())
+public class StausUser {
+public enum STATUS{REJECTED, PENDING, PROCESSING, APPROVED, NOT_VALID_CODE};
+
+private STATUS status;
+
+    StausUser(){
+        this.status = getStatus();
+    }
+    /**
+     * @return the status
+     */
+    public STATUS getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(STATUS status) {
+        this.status = status;
+    }
+    
+public void statusDetail(){
+    Scanner in =new Scanner(System.in);
+    System.out.println("Enter the user status code (zero,one,two,three) in string");
+    String code = in.next();
+    switch(code.toUpperCase())
         {
-        case "ZERO": System.out.println("REJECTED");
+        case "ZERO": System.out.println(STATUS.REJECTED);
         break;
-        case "ONE": System.out.println("PENDING");
+        case "ONE": System.out.println(STATUS.PENDING);
         break;
         case "TWO":
-        System.out.println("PROCESSING");
+        System.out.println(STATUS.PROCESSING);
         break;
         case "THREE": 
-            System.out.println("APPROVED");
+            System.out.println(STATUS.APPROVED);
         break;
         default:
-         System.out.println("NOT VALID CODE");
+         System.out.println(STATUS.NOT_VALID_CODE);
         break;
         }
-}
+    }
  
 }
